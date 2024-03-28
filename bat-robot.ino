8f6a3af8ba0c7030efb6fd3fe7b5a70ac5b15cea
Isaac Newcomb <isaacdnew@gmail.com>
2024-03-28 14:39:47 -0400

    trying to gitignore

--- bat-robot.ino
+++ bat-robot.ino
@@ -1,3 +1,5 @@
+#include <L293D.h>
+
 // This motor opens a door in a peek-a-boo toy.
 int waittime;
 int motorPin1 = 2; // 
@@ -14,10 +16,12 @@ void loop()
   // go forward for 0.5 seconds
   digitalWrite(motorPin1, HIGH);
   digitalWrite(motorPin2, LOW);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(100);
   
   // pause for 0.5 seconds to allow motor to stop
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, LOW);
-  delay(4900);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1900);
 }
